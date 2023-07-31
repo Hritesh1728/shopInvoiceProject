28390cc1a4e6c11e088c780b431e70b1f4117d58
Hritesh1728 <hriteshalw1728.com>
2023-08-01 00:00:44 +0530

    Project at it's last stages!

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {Component, OnDestroy, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import {Router} from "@angular/router";
 import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
-
+import {NgbOffcanvas} from "@ng-bootstrap/ng-bootstrap";
 
 
 @Component({
@@ -9,33 +9,44 @@ import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss'],
 })
+
 export class MainPageComponent implements OnInit {
   invoice_data: any[] = [];
   // total_form_fields = this.invoice_data.length;
   currentDate = new Date();
-
-  less_factor=20;
-  less_initiated =false ;
+  less_factor = 20;
+  less_initiated = false;
   del_index: number | null = null;
   del_item_initiated = false;
 
   total_price = 0;
   total_save_price = 0;
   open_invoice = false;
-  random4DigitNumber = 0;
-
+  random3DigitNumber = 0;
+  status_of_payment = 'Un-Paid';
+  paid_amount = 0;
+  remarked = false;
+  whatsapp_no = '';
 
 
   constructor(
     private router: Router,
     private modalService: NgbModal,
+    private offCanvas: NgbOffcanvas,
   ) {
   }
 
   ngOnInit() {
-    // Initialization logic goes here
-    console.log(this.invoice_data.length)
+    const retrievedArrayJSON = sessionStorage.getItem('invoice_data');
+    if (retrievedArrayJSON) {
+      this.invoice_data = JSON.parse(retrievedArrayJSON);
+      for (let i = 0; i < this.invoice_data.length; i++) {
+        this.total_price += this.invoice_data[i].product_price;
+        this.total_save_price += (this.invoice_data[i].product_mrp - this.invoice_data[i].product_price);
+      }
+    }
   }
+
 
   openDialog(addManually: TemplateRef<any>) {
     let last = this.invoice_data.length - 1;
@@ -48,6 +59,18 @@ export class MainPageComponent implements OnInit {
     }
     this.open_invoice = false;
     this.modalService.open(addManually, {centered: true, backdrop: 'static', animation: true});
+  }
+
+  transaction_content(panContent: TemplateRef<any>) {
+    this.offCanvas.open(panContent, {position: 'end', backdrop: 'static'});
+  }
+
+  about_content(about_me: TemplateRef<any>) {
+    this.offCanvas.open(about_me, {position: 'end', backdrop: 'static'});
+  }
+
+  share_invoice(whatsapp: TemplateRef<any>) {
+    this.offCanvas.open(whatsapp, {position: 'end', backdrop: 'static'});
   }
 
   increment_table() {
@@ -65,6 +88,7 @@ export class MainPageComponent implements OnInit {
         product_price: null,
       });
     }
+    this.del_item_initiated = false;
   }
 
   manual_data_submit() {
@@ -76,30 +100,35 @@ export class MainPageComponent implements OnInit {
     this.invoice_data[last].product_price = +(this.invoice_data[last].product_mrp).toFixed(2);
     this.total_price += this.invoice_data[last].product_price;
     this.total_save_price += (this.invoice_data[last].product_mrp - this.invoice_data[last].product_price);
+    this.del_item_initiated = false;
   }
 
   less_mrp() {
     let last = this.invoice_data.length - 1;
     this.less_initiated = false;
     if (this.less_factor != null && this.less_factor > 0) {
-      this.less_factor = 1-(+(this.less_factor/100).toFixed(2));
+      this.less_factor = 1 - (+(this.less_factor / 100).toFixed(2));
       this.invoice_data[last].product_price = +(this.invoice_data[last].product_mrp * this.less_factor).toFixed(2);
       this.total_price += this.invoice_data[last].product_price;
       this.total_save_price += (this.invoice_data[last].product_mrp - this.invoice_data[last].product_price);
     }
-    this.less_factor =20;
+    this.less_factor = 20;
+    this.del_item_initiated = false;
   }
 
   manual_data_reset() {
     this.invoice_data = [];
     this.total_price = 0;
     this.total_save_price = 0;
+    this.del_item_initiated = false;
+    this.remarked = false;
+    sessionStorage.clear();
   }
 
 
   openInvoice() {
     this.total_save_price = +this.total_save_price.toFixed(2);
-    this.random4DigitNumber = Math.floor(1000 + Math.random() * 9000);
+    this.random3DigitNumber = Math.floor(100 + Math.random() * 900);
     this.open_invoice = true;
   }
 
@@ -114,6 +143,45 @@ export class MainPageComponent implements OnInit {
   }
 
   openQrScan() {
+    const arrayJSON = JSON.stringify(this.invoice_data);
+    sessionStorage.setItem('invoice_data', arrayJSON);
     this.router.navigateByUrl(`qr_scanner`);
   }
+
+  protected readonly location = location;
+
+  whatsapp_invoice() {
+    const options: Intl.DateTimeFormatOptions = {
+      day: '2-digit', // 'dd' for two-digit day
+      month: '2-digit', // 'MM' for two-digit month
+      year: 'numeric' // 'yyyy' for full year
+    };
+    let return_price = (this.paid_amount - this.total_price) >= 0 ? (this.paid_amount - this.total_price) : 0;
+    const formattedDate: string = this.currentDate.toLocaleDateString(undefined, options);
+    let url_string = '?text=->%20%20%20%20%20%20%20%20*Invoice*%0D%0A' +
+      '%20%20*Token%20%20%20%20%20=%20%20%20%20%20' + this.random3DigitNumber + '*%0D%0A' +
+      '%20%20Date%20%20%20%20%20%20%20=%20%20%20%20%20' + formattedDate + '%0D%0A' +
+      '%20%20------------------------------------%0D%0A';
+    for (let i = 0; i < this.invoice_data.length; i++) {
+      let spaces = 30 - (this.invoice_data[i].product_name.length-1) * 2;
+      url_string += '%20%20' + this.invoice_data[i].product_name;
+      for (let j = 0; j < spaces; j++) url_string += '%20';
+      url_string += this.invoice_data[i].product_mrp + '%20%20%20%20' + this.invoice_data[i].product_price + '%0D%0A';
+    }
+    url_string += '%20%20------------------------------------%0D%0A' +
+      '%20%20Total+Price%20%20%20%20%20=%20%20%20%20%20' + this.total_price + '%0D%0A' +
+      '%20%20Saved+Price%20%20%20%20%20=%20%20%20%20%20' + this.total_save_price + '%0D%0A';
+    if (this.status_of_payment == 'Paid') {
+      url_string += '%20%20Status%20%20%20%20%20=%20%20%20%20%20' + this.status_of_payment + '%0D%0A' +
+        '%20%20Paid+Amount_by+customer%20%20%20%20%20=%20%20%20%20%20' + this.paid_amount + '%0D%0A' +
+        '%20%20~Return~%20%20%20%20%20=%20%20%20%20%20' + return_price + '%0D%0A' +
+        '%20%20Thank+you+for+Shopping+with+us%20%20%20%20%20%20%20%20%20%20';
+    } else {
+      url_string += '%20%20Status%20%20%20%20%20=%20%20%20%20%20' + this.status_of_payment + '%0D%0A' +
+        '%20%20Thank+you+for+Shopping+with+us%20%20%20%20%20%20%20%20%20%20';
+    }
+    window.open('https://wa.me/91' + this.whatsapp_no + url_string, '_blank');
+  }
 }
+
+
--- src/app/qr-code-scan/qr-code-scan.component.ts
+++ src/app/qr-code-scan/qr-code-scan.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {
   ScannerQRCodeConfig,
   ScannerQRCodeResult,
@@ -6,13 +6,15 @@ import {
   NgxScannerQrcodeComponent,
   ScannerQRCodeSelectedFiles,
 } from 'ngx-scanner-qrcode';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-qr-code-scan',
   templateUrl: './qr-code-scan.component.html',
   styleUrls: ['./qr-code-scan.component.scss']
 })
-export class QrCodeScanComponent {
+
+export class QrCodeScanComponent implements OnInit,OnDestroy {
   // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia#front_and_back_camera
   public config: ScannerQRCodeConfig = {
     constraints: {
@@ -20,36 +22,76 @@ export class QrCodeScanComponent {
         width: window.innerWidth
       },
     },
-    // canvasStyles: [
-    //   { /* layer */
-    //     lineWidth: 1,
-    //     fillStyle: '#00950685',
-    //     strokeStyle: '#00950685',
-    //   },
-    //   { /* text */
-    //     font: '17px serif',
-    //     fillStyle: '#ff0000',
-    //     strokeStyle: '#ff0000',
-    //   }
-    // ],
+    canvasStyles: [
+      { /* layer */
+        lineWidth: 1,
+        fillStyle: '#00950685',
+        strokeStyle: '#00950685',
+      },
+      { /* text */
+        font: '17px serif',
+        fillStyle: '#ff0000',
+        strokeStyle: '#ff0000',
+      }
+    ],
   };
-
-  public qrCodeResult: ScannerQRCodeSelectedFiles[] = [];
-  public qrCodeResult2: ScannerQRCodeSelectedFiles[] = [];
+  qr_string= '';
+  invoice_data:any[] = [];
 
   @ViewChild('action') action!: NgxScannerQrcodeComponent;
 
-  constructor(private qrcode: NgxScannerQrcodeService) { }
+  constructor(private qrcode: NgxScannerQrcodeService,
+              private router: Router
+  ) {
+  }
 
-  ngAfterViewInit(): void {
-    this.action.isReady.subscribe((res: any) => {
-      // this.handle(this.action, 'start');
-    });
+  ngOnInit() {
+    const myButton = document.getElementById('camera_button');
+    if (myButton) myButton.click();
+  }
+  ngOnDestroy() {
+   location.reload();
   }
 
   public onEvent(e: ScannerQRCodeResult[], action?: any): void {
-    // e && action && action.pause();
-    console.log(e);
+    // console.log(e);
+
+    if(e){
+      this.qr_string = e[0]['value'];
+      const retrievedArrayJSON = sessionStorage.getItem('invoice_data');
+      if(retrievedArrayJSON){
+        this.invoice_data = JSON.parse(retrievedArrayJSON);
+        // console.log("here get item from session storage", this.invoice_data);
+      }
+      let amount = 0, count = 0;
+      if (this.qr_string) {
+        let n = this.qr_string.length;
+        for (let i = n - 1; i >= 0; i--) {
+          const charCode = this.qr_string.charCodeAt(i);
+          if (this.qr_string[i] == ' ' && amount != 0) {
+            this.qr_string = this.qr_string.slice(0, i);
+            break;
+          }
+          if (charCode >= 48 && charCode <= 57) {
+            amount += (+this.qr_string[i]) * Math.pow(10, count);
+            count++;
+          }
+        }
+      }
+      let last= this.invoice_data.length - 1;
+      if(last>=0 && this.invoice_data[last].product_price==null) this.invoice_data.pop();
+      this.invoice_data.push(
+        {
+          product_name: this.qr_string,
+          product_mrp: amount,
+          product_price: amount,
+        }
+      );
+      const arrayJSON = JSON.stringify(this.invoice_data);
+      sessionStorage.setItem('invoice_data', arrayJSON);
+      this.router.navigateByUrl('main_page');
+    }
+
   }
 
   public handle(action: any, fn: string): void {
@@ -65,22 +107,5 @@ export class QrCodeScanComponent {
     } else {
       action[fn]().subscribe((r: any) => console.log(fn, r), alert);
     }
-  }
-
-  public onDowload(action: NgxScannerQrcodeComponent) {
-    action.download().subscribe(console.log, alert);
-  }
-
-  public onSelects(files: any) {
-    this.qrcode.loadFiles(files, 0.5).subscribe((res: ScannerQRCodeSelectedFiles[]) => {
-      this.qrCodeResult = res;
-    });
-  }
-
-  public onSelects2(files: any) {
-    this.qrcode.loadFilesToScan(files, this.config, 0.5).subscribe((res: ScannerQRCodeSelectedFiles[]) => {
-      console.log(res);
-      this.qrCodeResult2 = res;
-    });
   }
 }
